8414064ce6ad544b162cb42a28920b79e96067a8
Edgar Alberto Alvarez Garcia <edgar.alvarez@suramexico.com>
2020-02-02 14:25:33 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strinct";
+
+const { buildSchema } = require("graphql");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
+
+const app = express();
+const port = process.env.port || 3000;
+
+//  definiendo el esquema
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
+
+app.use(
+  "/api",
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`server is listening at http://localhost:${port}`);
+});

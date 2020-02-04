0ca9010b70df3a5a576fa9cc0a534f6225898264
Edgar Alberto Alvarez Garcia <edgar.alvarez@suramexico.com>
2020-02-03 18:41:23 -0600

    back

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-"use strinct";
-
-const { buildSchema } = require("graphql");
-const express = require("express");
-const gqlMiddleware = require("express-graphql");
-const { readFileSync } = require("fs");
-const { join } = require("path");
-const resolvers = require("./lib/resolvers");
+require('dotenv').config();
+const { makeExecutableSchema } = require('graphql-tools');
+const express = require('express');
+const gqlMiddleware = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.port || 3000;
 
-//  definiendo el esquema
-const schema = buildSchema(
-  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+// definiendo el esquema
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 );
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 app.use(
-  "/api",
+  '/api',
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
@@ -25,5 +26,5 @@ app.use(
 );
 
 app.listen(port, () => {
-  console.log(`server is listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}/api`);
 });
